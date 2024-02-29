e33d23e080f1a809a032ad1d6e7a1d44d3378c7d
wsch481 <wsch481@gmail.com>
2024-02-28 21:58:12 -0500

    bye

--- learning_rust/target/debug/deps/learning_rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\wschu\Documents\gitresp\learning_rust\target\debug\deps\learning_rust.exe: src\main.rs
-
-C:\Users\wschu\Documents\gitresp\learning_rust\target\debug\deps\learning_rust.d: src\main.rs
-
-src\main.rs:
--- learning_rust/target/debug/learning_rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\wschu\Documents\gitresp\learning_rust\target\debug\learning_rust.exe: C:\Users\wschu\Documents\gitresp\learning_rust\src\main.rs
