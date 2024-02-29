e441fa0e66dfc741dd8016da30644c8bd9a75648
wsch481 <wsch481@gmail.com>
2024-02-28 20:59:46 -0500

    Learning Rust Course

---
+++ learning_rust/target/debug/deps/learning_rust.d
@@ -0,0 +1,5 @@
+C:\Users\wschu\Documents\gitresp\learning_rust\target\debug\deps\learning_rust.exe: src\main.rs
+
+C:\Users\wschu\Documents\gitresp\learning_rust\target\debug\deps\learning_rust.d: src\main.rs
+
+src\main.rs:
---
+++ learning_rust/target/debug/learning_rust.d
@@ -0,0 +1,1 @@
+C:\Users\wschu\Documents\gitresp\learning_rust\target\debug\learning_rust.exe: C:\Users\wschu\Documents\gitresp\learning_rust\src\main.rs
